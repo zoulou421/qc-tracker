12870999f7239b8b023549a4f0185fb39ff7b324
Bonevy BEBY <bonevybeby@gmail.com>
2025-04-08 11:31:13 +0100

    Update models

--- qc_addons/qctracker/models/__init__.py
+++ qc_addons/qctracker/models/__init__.py
@@ -11,9 +11,5 @@ from . import qc_tracker_skill
 from . import qc_tracker_skill_rating
 from .import qc_tracker_task_notification
 from .import qc_tracker_dashboard
-<<<<<<< HEAD
 
 
-=======
-from .import dashboard_dummy
->>>>>>> 007482e113be1765a02e12bdbb56096f4a1790b3
